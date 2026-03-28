f77f79ad3eac2062ce85398ced41305fcb336ff0
orpheseum <jimmiedavey@proton.me>
2026-03-28 13:35:42 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('dream-v1').then(c => c.addAll(['/', '/index.html', '/style.css', '/app.js'])));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
