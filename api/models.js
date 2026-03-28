e0ade6802147c6970cad84da5140d834bd12d7f9
orpheseum <jimmiedavey@proton.me>
2026-03-28 12:41:28 +0000

    Delete api/models.js

--- api/models.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const apiKey = process.env.GEMINI_API_KEY;
-  const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-}
